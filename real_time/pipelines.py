69650b10757bbe52e2f7d2cadbfce56ec4d1dbfd
djwang <wangdejie1996@gmail.com>
2017-07-01 11:45:57 +0800

    保存在xlsx

--- real_time/pipelines.py
+++ real_time/pipelines.py
@@ -6,18 +6,18 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from openpyxl import Workbook
 
-class RealTimePipeline(object):
-    def process_item(self, item, spider):
-        pass
-
-
 class DetailPipeline(object):
-    wb = Workbook()
-    ws = wb.active
-    ws.append(['注册号','国际分类','申请日期','注册日期','申请人名称','初审公告期号','初审公告日期','注册公告期号','注册公告日期','专用权期限','商标类型','是否共有商标','备注','代理人名称','商品/服务','类似群组','商标流程'])
+    def __init__(self):
+        self.wb = Workbook()
+        self.ws = self.wb.active
+        self.ws.append(
+            ['注册号', '国际分类', '申请日期', '注册日期', '申请人名称', '初审公告期号', '初审公告日期', '注册公告期号', '注册公告日期', '专用权期限', '商标类型', '是否共有商标',
+             '备注', '代理人名称', '商品/服务', '类似群组', '商标流程'])
 
     def process_item(self, item, spider):
-        line = [item['RegistrationNumber'],item['Classification'], item['ApplyDate'], item['RegisterDate']]
+        #pipelines主要用于数据的进一步处理，比如类型转换、存储入数据库、写到本地等。
+        #类型转换
+        line = [item['RegistrationNumber'][0]]
         self.ws.append(line)
-        self.ws.save('../BrandInfo.xlsx')
+        self.wb.save('BrandInfo.xlsx')
         return item
--- real_time/spiders/BrandInfoSpider.py
+++ real_time/spiders/BrandInfoSpider.py
@@ -1,24 +1,25 @@
 import scrapy
 from scrapy.selector import Selector
 from .. import items
+from openpyxl import Workbook
+from scrapy import log
 class BrandInfoSpider(scrapy.Spider):
     name = "BrandInfo"
-    start_urls=["http://www.tm.cn/search"]
 
+    def __init__(self, value=None, searchType=None):
+        super(BrandInfoSpider, self).__init__()
+        self.start_urls = ['http://www.tm.cn/search?value=%s&searchType=%s' %(value, searchType)]
 
     def parse(self, response):
-        #collect 'brand_urls'
         for detailHref in response.xpath('//tbody//tr//@href'):
             keyAndValue = detailHref.extract()
             if keyAndValue is not None:
-                next_page = response.urljoin(keyAndValue)
+                next_page = 'http://www.tm.cn'+keyAndValue
                 yield scrapy.Request(next_page, callback=self.parse_Detail)
 
 
-
-
     def parse_Detail(self, response):
-        item = items.RealTimeTime()
+        item = items.RealTimeItem()
         tbodys = response.xpath('//tbody')
 
         sel = Selector(text=tbodys[0].extract())
@@ -28,6 +29,11 @@ class BrandInfoSpider(scrapy.Spider):
         item['Classification'] = sel.xpath('//tr[2]//td[last()]/text()').extract()
         item['ApplyDate'] = sel.xpath('//tr[3]//td[last()]/text()').extract()
         item['RegisterDate'] = sel.xpath('//tr[4]//td[last()]/text()').extract()
+
+        file = open('temp.txt','wb')
+        with file as fp:
+            if len(item['RegistrationNumber']) != 0:
+                fp.write(item['RegistrationNumber'][0])
 
         yield item
 
